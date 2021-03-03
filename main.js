677f915a64d0c3d32f2b6becb63592887ce05ef8
Pranav Suri <pranavsuri.433@gmail.com>
2021-03-04 01:05:30 +0800

    aasdasd

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if ((message.author.bot == false) && (message.content.includes("Not Rocket Destroyer"))) {
+  if ((message.author.bot == false)) {
     message.channel.send(message.content);
+    console.log(message);
   }
 });
-
+// && (message.content.includes("Not Rocket Destroyer"))
 client.login("ODA5MTE2MDkwNjkyOTI3NTM2.YCQaFA.KrSNxh1iKyqLMlz_ig9ebr7gWks");
