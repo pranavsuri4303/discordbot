c43bb7ffffc3f88d2b24d9977b60c20db7ea4184
Pranav Suri <pranavsuri.433@gmail.com>
2021-03-04 01:17:12 +0800

    ssdfs

--- main.js
+++ main.js
@@ -9,8 +9,5 @@ client.on("message", (message) => {
   if (message.mentions.users.has("720902882928361543") && message.author.bot == false) {
     message.reply("Stop tagging Not Rocket Destroyer you fool.");
   }
-  if (message.mentions.users.has("797138627044835328") && message.author.bot == false){
-    message.reply("Stop tagging Rocket Destroyer you fool.");
-  }
 });
 client.login("ODA5MTE2MDkwNjkyOTI3NTM2.YCQaFA.KrSNxh1iKyqLMlz_ig9ebr7gWks");
