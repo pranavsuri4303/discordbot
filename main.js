e9d04971c3bb0fe89e7e15599f07beb132c6778c
Pranav Suri <pranavsuri.433@gmail.com>
2021-03-04 00:21:06 +0800

    abcd

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ const chessAPI = new ChessWebAPI();
 const Discord = require('discord.js');
 const client = new Discord.Client();
  
-const prefix = '*';
+// const prefix = '*';
  
 const fs = require('fs');
  
-client.commands = new Discord.Collection();
+// client.commands = new Discord.Collection();
  
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+// const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+// for(const file of commandFiles){
+//     const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
-}
+//     client.commands.set(command.name, command);
+// }
  
  
 client.once('ready', () => {
