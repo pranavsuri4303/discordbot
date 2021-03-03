4f1c9909639e68ffc8a91883273fd41fee380740
Pranav Suri <pranavsuri.433@gmail.com>
2021-03-04 00:19:34 +0800

    test new command

--- main.js
+++ main.js
@@ -24,19 +24,22 @@ client.once('ready', () => {
 });
  
 client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
-    if (!client.commands.has(commandName)) return;
-    const command = client.commands.get(commandName);
-    try {
-        command.execute(message, args);
-
-    } 
-    catch (error) {
-	    console.error(error);
-        message.reply('there was an error trying to execute that command!');
+    if (message.content.includes("Not Rocket Destroyer")){
+        message.reply('Destroyed');
     }
+    // if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    // const args = message.content.slice(prefix.length).split(/ +/);
+    // const commandName = args.shift().toLowerCase();
+    // if (!client.commands.has(commandName)) return;
+    // const command = client.commands.get(commandName);
+    // try {
+    //     command.execute(message, args);
+
+    // } 
+    // catch (error) {
+	//     console.error(error);
+    //     message.reply('there was an error trying to execute that command!');
+    // }
 });
 client.login('Nzk3MTM4NjI3MDQ0ODM1MzI4.X_iHMg.68EqW4DID-VJjMgGzGI-PFV45jc');
