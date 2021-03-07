d52aada08bf6024c3c0a5b910454be23615f8c1d
Pranav Suri <pranavsuri.433@gail.com>
2021-03-07 17:52:40 +0800

    asdsad

--- main.js
+++ main.js
@@ -6,11 +6,20 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if ((message.mentions.users.has("809116090692927536") || message.mentions.users.has("720902882928361543") || message.mentions.users.has("670816688891101204")) && message.author.bot == false) {
+  if ((message.mentions.users.has("809116090692927536") || 
+  message.mentions.users.has("720902882928361543") || 
+  message.mentions.users.has("670816688891101204")) && 
+  message.author.bot == false) {
     message.reply("Stop tagging Rocket Destroyer you fool.");
   }
   if (message.content.includes("nitro")){
     message.reply("No Nitro for SIMPS on this server!");
+  }
+  if (message.content.includes("rocket")){
+    message.reply("How dare you mention rocket!");
+  }
+  if (message.content.includes("@everyone") && (message.author != 670816688891101204)){
+    message.reply("Everyone tag karna band kar!");
   }
 });
 client.login("ODA5MTE2MDkwNjkyOTI3NTM2.YCQaFA.KrSNxh1iKyqLMlz_ig9ebr7gWks");
